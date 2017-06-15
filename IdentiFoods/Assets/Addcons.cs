1687834ec222aa5912d2c7a53c72b28ea9b12162
abhimanyudhawan <ad3980@nyu.edu>
2017-06-15 02:25:34 -0400

    IdentiFood Basic UI Mainscreen
    Added Button to initiate Basic UI

---
+++ IdentiFoods/Assets/Addcons.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Addcons : MonoBehaviour {
+    public TextMeshProUGUI format;
+    public string[] cons = { "Bitter", "High Fat", "Low Sodium" } ;
+    private bool addheading = true;
+
+    public void Addnew(string con)
+    {
+        if (addheading)
+        {
+            TextMeshProUGUI proheading = TextMeshProUGUI.Instantiate(format, this.transform, false);
+            proheading.text = "Cons";
+            addheading = false;
+        }
+        for (int i = 0; i < cons.Length; i++)
+        {
+            TextMeshProUGUI con1 = TextMeshProUGUI.Instantiate(format, this.transform, false);
+            con1.text = cons[i];
+        }
+
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ IdentiFoods/Assets/Addpros.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Addpros : MonoBehaviour {
+    public TextMeshProUGUI format;
+    private bool addheading = true;
+    public string[] pros = { "Tasty", "High Calcium", "Low fructose" };
+
+    public void Addnew(string pro)
+    {
+        if (addheading)
+        {
+            TextMeshProUGUI proheading = TextMeshProUGUI.Instantiate(format, this.transform, false);
+            proheading.text = "Pros";
+            addheading = false;
+        }
+        for (int i = 0; i < pros.Length; i++)
+        {
+            TextMeshProUGUI pro1 = TextMeshProUGUI.Instantiate(format, this.transform, false);
+            pro1.text = pros[i];
+            Debug.Log(format.GetComponent<TextMeshProUGUI>().text);
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
